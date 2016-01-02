77f2d2b70bbbfe3c5eef3903ea9e1b2b4edf2f63
krasch <krasch@kth.se>
2016-01-03 00:03:38 +0100

    Email encryption

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import yaml
 import pdb
+
 from pgpbuddy.fetch import fetch_messages
 from pgpbuddy.send import send_message
+from pgpbuddy.crytpo import decrypt_message
 
 with open("config.yaml", 'r') as config:
     config = yaml.load(config)
     messages = fetch_messages(config["pop3-server"], config["username"], config["password"])
     for msg in messages:
-        target = msg['From']
+        #target = msg['From']
 
-        if msg['Content-Type'].split(';')[0] == 'text/plain':
-            send_message(config["smtp-server"], config["smtp-port"], config["username"], config["password"], target)
+        #if msg['Content-Type'].split(';')[0] == 'text/plain':
+        #    send_message(config["smtp-server"], config["smtp-port"], config["username"], config["password"], target)
+        decrypt_message(config["gnupghome"], msg)
