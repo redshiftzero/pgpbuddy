21001f31b7cf4e992fa30e240aa3958a1058b362
krasch <krasch@kth.se>
2016-01-03 17:59:32 +0100

    Checking email signatures works

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
-arsch
 # pgpbuddy
 Baby's first PGP encrypted email buddy! 
 
@@ -10,3 +9,10 @@ Baby's first PGP encrypted email buddy!
 
     cp config.default.yaml config.yaml
     # and set your email server parameters
+    
+    # make a keyring that only contains pgpgbuddys key
+    # set path/to/new-keyring.gpg in config file
+    gpg --keyring pubring.gpg --export BUDDYKEY > /tmp/exported.key
+    gpg --no-default-keyring --keyring=buddyring.gpg --import /tmp/exported.key
+    $ gpg --export-secret-keys -a keyid > my_private_key.asc
+    $ gpg --export -a keyid > my_public_key.asc
--- main.py
+++ main.py
@@ -3,12 +3,12 @@ import pdb
 from pgpbuddy.fetch import fetch_messages
 from pgpbuddy.send import (plaintext_response, encrypted_response,
                            signed_response, encryptsigned_response)
-from pgpbuddy.crytpo import init_gpg, is_encrypted, is_signed, decrypt_message
+from pgpbuddy.crypto import init_gpg, is_encrypted, is_signed, decrypt, verify_signature, import_public_key
 
 
 def select_response(gpg, msg):
     if is_encrypted(msg):
-        decrypted_message = decrypt_message(gpg, msg)
+        decrypted_message = decrypt(gpg, msg)
 
         if is_signed(decrypted_message):
             ## check valid sig TODO
@@ -17,7 +17,8 @@ def select_response(gpg, msg):
             return encrypted_response
 
     elif is_signed(msg):
-        ## check valid sig TODO
+        import_public_key(gpg, msg["From"])
+        verify_signature(gpg, msg)
         return signed_response
 
     elif msg['Content-Type'].split(';')[0] == 'text/plain':
@@ -29,13 +30,12 @@ def select_response(gpg, msg):
 def load():
     with open("config.yaml", 'r') as config:
         config = yaml.load(config)
-        gpg = init_gpg(config["gnupghome"])
-
-        messages = fetch_messages(config["pop3-server"], config["username"], config["password"])
-        for msg in messages:
-            target = msg["From"]
-            respond = select_response(gpg, msg)
-            respond(config["smtp-server"], config["smtp-port"], config["username"], config["password"], target)
+        with init_gpg(config["gnupghome"]) as gpg:
+            messages = fetch_messages(config["pop3-server"], config["username"], config["password"])
+            for msg in messages:
+                target = msg["From"]
+                respond = select_response(gpg, msg)
+                #respond(config["smtp-server"], config["smtp-port"], config["username"], config["password"], target)
 
     return None
 
