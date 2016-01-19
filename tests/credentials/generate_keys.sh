5f7d8c7adc49bb73e5cc179eb515ba61d961c558
krasch <krasch@kth.se>
2016-01-19 23:52:33 +0100

    Auditing encrypt+signed cases

--- tests/credentials/generate_keys.sh
+++
@@ -1,8 +0,0 @@
-mkdir buddy
-gpg --homedir buddy --batch --gen-key buddy.keyconfig
-
-mkdir user1
-gpg --homedir user1 --batch --gen-key user1.keyconfig
-
-mkdir user2
-gpg --homedir user2 --batch --gen-key user2.keyconfig
