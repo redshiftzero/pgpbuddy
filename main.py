e6808c09e1e2f185abf49969b1bff2589779409b
redshiftzero <jen@redshiftzero.com>
2016-01-24 00:14:35 -0600

    check messages every min

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import yaml
 import pdb
+import schedule
+import time
 
 from pgpbuddy import buddy
 
@@ -7,4 +9,7 @@ from pgpbuddy import buddy
 if __name__ == '__main__':
     with open("config.yaml", 'r') as config:
         config = yaml.load(config)
-        buddy.check_and_reply_to_messages(config)
+        schedule.every(1).minutes.do(buddy.check_and_reply_to_messages(config))
+        while True:
+            schedule.run_pending()
+            time.sleep(20)    
