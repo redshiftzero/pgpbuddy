1e068287d5dda5e2c4151b52dfd9d2b9cf29c89e
krasch <krasch@kth.se>
2016-01-29 21:20:51 +0100

    Move scheduling outside of config loading scope

--- main.py
+++ main.py
@@ -9,7 +9,8 @@ from pgpbuddy import buddy
 if __name__ == '__main__':
     with open("config.yaml", 'r') as config:
         config = yaml.load(config)
-        schedule.every(1).minutes.do(buddy.check_and_reply_to_messages(config))
-        while True:
-            schedule.run_pending()
-            time.sleep(20)    
+
+    schedule.every(1).minutes.do(buddy.check_and_reply_to_messages(config))
+    while True:
+        schedule.run_pending()
+        time.sleep(20)
