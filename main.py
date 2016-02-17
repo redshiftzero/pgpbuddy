945a01cb61b246c837d4a7c46522c2925e46d05e
redshiftzero <jen@redshiftzero.com>
2016-02-17 11:47:10 -0600

    get rid of schedule

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import yaml
 import pdb
-import schedule
 import time
 
 from pgpbuddy import buddy
@@ -10,7 +9,6 @@ if __name__ == '__main__':
     with open("config.yaml", 'r') as config:
         config = yaml.load(config)
 
-    schedule.every(1).minutes.do(buddy.check_and_reply_to_messages(config))
     while True:
-        schedule.run_pending()
-        time.sleep(20)
+        buddy.check_and_reply_to_messages(config)
+        time.sleep(60)
